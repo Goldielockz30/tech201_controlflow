aea1946bb1bcc3eae7fd3bd6dca0c0252dc8ccd7
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-01-31 17:44:40 +0000

    movies

--- my_movie_ratings.py
+++ my_movie_ratings.py
@@ -1,21 +1,24 @@
 
 
 
+# age = input("What is your age?")
+# if age.isdigit() and int(age) < 117 or int(age) < 1:
+#    print(f"You are {age} years old ")
+#
+# else:
+#    print("Please provide your answer in digits and below 117")
+#
 age = input("What is your age?")
-if age.isdigit() and int(age) < 117:
-   print(f"You are {age} years old ")
 
-else:
-   print("Please provide your answer in digits and below 117")
-
-age = input("What is your age?")
-
-if age <= "11":
+if int(age.isdigit()) and int(age) <= int("11"):
     print("You can watch U rated movies and PG")
-elif int(age) == int("12") or int(age) < int("18"):
+elif int(age.isdigit()) == int("12") or int(age) < int("18"):
     print("You can watch U, PG and 12 rated movies")
-elif int(age) >= int("18"):
+elif int(age.isdigit()) >= int("18"):
     print("You can watch all movies since you are 18 or over")
+else:
+    print("Please provide your answer in digits and below 117")
+
 
 
 # elif age == "15":
