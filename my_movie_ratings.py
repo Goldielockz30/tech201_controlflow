15fc6f055b38db0678b6357c56e555764aee4c0e
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-01-31 15:47:21 +0000

    movies

--- my_movie_ratings.py
+++ my_movie_ratings.py
@@ -8,17 +8,17 @@ if age.isdigit() and int(age) < 117:
 else:
     print("Please provide your answer in digits and below 117")
 
-film_rating = "18"
+film_rating = input()
 
-if age.lower() == "universal":
+if film_rating.lower() == "universal":
      print("All age groups can watch this film")
-elif age.lower() == "pg":
+elif film_rating.lower() == "pg":
     print("General viewing but parental guidance advised")
-elif age.lower() == "12" or age == "12a":
+elif film_rating.lower() == "12" or film_rating == "12a":
    print("12 rated movies may not be suitable for those under 12, but supervision is recommended")
-elif age.lower() == "15":
+elif film_rating.lower() == "15":
    print("You must be 18 to watch 15 rated movies in the cinema")
-elif age.lower() == "18":
+elif film_rating.lower() == "18":
    print("You can watch this movie since you are 18 or over")
 else:
   print("This is not a correct rating, please use universal, pg, 12, 15, 18")
