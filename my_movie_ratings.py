03bb7802ac48dd5b4437b192e30af39026644b52
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-02-01 16:37:02 +0000

    movies

--- my_movie_ratings.py
+++ my_movie_ratings.py
@@ -5,7 +5,7 @@ if int(age.isdigit()) and int(age) > int("117") and int(age) != int("0"):
     print("Please provide your answer in digits, with age 1 and above and below 117")
 elif int(age.isdigit()) and int(age) <= int("11") and int(age) != int("0"):
     print("You can watch U rated movies and PG")
-elif int(age.isdigit()) and int(age) == int("12") and int(age) < int("18"):
+elif int(age.isdigit()) and int(age) >= int("12") and int(age) < int("18"):
     print("You can watch U, PG and 12 rated movies")
 elif int(age.isdigit()) and int(age) >= int("18") and int(age) > int("1"):
     print("You can watch all movies since you are 18 or over")
