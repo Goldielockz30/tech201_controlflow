5dc5346bf6c634922707124ca9207e177ea00fa0
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-02-01 17:36:14 +0000

    moviefunctions

--- movies_with_functions.py
+++ movies_with_functions.py
@@ -1,18 +1,17 @@
 
+def movie_program():
+    age = int(input("How old are you? "))
+    if int(age) > 0 and int(age) < 117:
+        if int(age) >= 18:
+            print("You are old enough to but a ticket for any movie")
+        elif int(age) < 18 and int(age) >= 15:
+            print("You can watch 15, 12, PG and universal rated movies")
+        elif int(age) < 15 and int(age) >= 12:
+            print("You can watch 15, 12, PG and universal rated movies")
+        else:
+            print("You can buy tickets for U and PG movies")
+    else:
+        print("Please enter a valid number for your age")
+movie_program()
+movie_program()
 
-def age(num1: int, num2: int) -> float:
-    return num1 + num2
-age = int(input("How old are you: "))
-num2 = int(input("Enter another number: "))
-print(addition(num1, num2))
-
-if int(age.isdigit()) and int(age) > int("117") and int(age) != int("0"):
-    print("Please provide your answer in digits, with age 1 and above and below 117")
-elif int(age.isdigit()) and int(age) <= int("11") and int(age) != int("0"):
-    print("You can watch U rated movies and PG")
-elif int(age.isdigit()) and int(age) >= int("12") and int(age) < int("18"):
-    print("You can watch U, PG and 12 rated movies")
-elif int(age.isdigit()) and int(age) >= int("18") and int(age) > int("1"):
-    print("You can watch all movies since you are 18 or over")
-else:
-    print("Please provide your answer in digits, with age 1 and above and below 117")
