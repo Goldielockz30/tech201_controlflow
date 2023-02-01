54613e653e1cbbb4c561409e7311e3ca667d0ddb
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-02-01 20:20:26 +0000

    pay

--- pay.py
+++ pay.py
@@ -1,15 +1,12 @@
 
 
+def pay_program(hours, pay):
+
+        return hours, pay
 
 hours = input("How many hours have you worked this week? ")
-rate = input("How much do you get paid by the hour? ")
-over = input("How many hours of over time? ")
-pay = 40 * int(rate)
-overtime = int(rate) * 1.5 * int(over) + pay
-underpay = int(rate) * int(hours)
-if hours == "40":
-    print(f"You have worked {hours} hours and you will be paid {pay}")
-elif hours > "40":
-    print(f"you have worked {hours} hours and you will be paid {overtime}")
-elif hours < "40":
-    print(f"You have worked {hours} hours you will be paid {underpay}")
+pay = input("How much do you get paid by the hour? ")
+
+paycheck = pay_program(int(hours), int(pay))
+total = int(hours) * int(pay)
+print(f"Total gross pay: {total} ")
